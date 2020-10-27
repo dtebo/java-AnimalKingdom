c0b4652c29aec6f92fa37c4b4bd6ff75591b8648
Darren Tebo <darrentebo83@outlook.com>
2020-10-27 15:18:42 -0400

    completed animals abstract class

---
+++ src/animalkingdom/Animals.java
@@ -0,0 +1,45 @@
+package animalkingdom;
+
+public abstract class Animals {
+    protected static int maxID = 0;
+    protected int id;
+    protected int foodAmount = 0;
+    protected String name;
+    protected String yearDiscovered;
+
+    public Animals(String name, String yearDiscovered){
+        maxID++;
+        this.id = maxID;
+        this.name = name;
+        this.yearDiscovered = yearDiscovered;
+    }
+
+    public int getFoodAmount(){
+        return this.foodAmount;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getYearDiscovered(){
+        return this.yearDiscovered;
+    }
+
+    public void setFoodAmount(int amt){
+        this.foodAmount = amt;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setYearDiscovered(String yearDiscovered){
+        this.yearDiscovered = yearDiscovered;
+    }
+
+    @Override
+    public String toString(){
+        return this.name + " " + this.yearDiscovered;
+    }
+}
